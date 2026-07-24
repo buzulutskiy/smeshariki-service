82183fdb5a7fff5dc9224025c2893da340f8a596
Anton Buzulutskiy <abvinchi@gmail.com>
2026-07-24 10:47:14 +0300

    Initial commit: Smeshariki series catalog with 703 episodes, 12 categories, search and watched marks

---
+++ server.py
@@ -0,0 +1,19 @@
+import http.server
+import os
+import socketserver
+import sys
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+os.chdir(ROOT)
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8742
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=ROOT, **kwargs)
+
+
+socketserver.TCPServer.allow_reuse_address = True
+with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+    print(f"serving {ROOT} at http://127.0.0.1:{PORT}", flush=True)
+    httpd.serve_forever()
